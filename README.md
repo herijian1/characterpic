e034dff1761522b7d2237dc0c7f10573db9d19ad
herijian <351302146@qq.com>
2025-11-29 21:49:38 +0800

    清理历史提交记录并升级面板图更新插件

--- README.md
+++ README.md
@@ -1,37 +1,105 @@
-# 优质 喵喵插件原神星铁角色面板图
+<h1>何日见原铁面板图主仓库</h1>
 
-#### 介绍
-喵喵插件原神星铁优质面板图
+> 原仓库存储上限了，搞了一大堆不必要的，故对本仓库进行了整理，存储面板图的仓库更改为[面板图存储库](https://gitee.com/herijian/characterpic1)，因仓库地址改变，需要重新克隆存储仓库，而不是本仓库
 
-#### 软件架构
-软件架构说明
+啥都聊的交流群[768216689](http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=PQAshGBsq9gi1PPGrsCkHdz47Pj1zhm2&authKey=qcuZzat49QZoHvN%2FNFMp5XaDRK4l2Ngo7lz%2B9USyqEnMpLFZH4CyqqHId6mv9ZwG&noverify=0&group_code=768216689)
 
+<div align="center">
+  <img src="mascot.jpg" alt="mascot" width = "400">
+  </a><br>
 
-#### 安装教程
+<h1>喵喵插件miao-plugin</h1>
 
-1.  xxxx
-2.  xxxx
-3.  xxxx
+[![插件库](https://img.shields.io/badge/Gitee-插件库-black?style=flat-square&logo=gitee)](https://gitee.com/yhArcadia/Yunzai-Bot-plugins-index) [![Mioao-Yunzai](https://img.shields.io/badge/Miao--Yunzai-V3-orange.svg)](https://github.com/yoimiya-kokomi/Miao-Yunzai) [![TRSS-Yunzai](https://img.shields.io/badge/TRSS--Yunzai-V3-orange.svg)](https://github.com/TimeRainStarSky/Yunzai) [![miao-plugin](https://img.shields.io/badge/喵喵-面板-black?style=flat-square&logo=dependabot)](https://gitee.com/yoimiya-kokomi/miao-plugin) [![Group](https://img.shields.io/badge/群号-768216689-red?style=flat-square&logo=GroupMe&logoColor=white)](https://qun.qq.com/universal-share/share?ac=1&authKey=OhXBwpnRd2oyFNIbE%2BItRHYAGj8JpSiWFVBnb4tEz6lJzXxnRzQytnMaHSEWFE4z&busi_data=eyJncm91cENvZGUiOiI3NjgyMTY2ODkiLCJ0b2tlbiI6ImNsYzl2MFNBSGxkOWFJcDJ5YnpKam9BZFZXdkFOZ0MveVliZE1pU0VVbHNqQnZGdlYvSTRvcmpPbUs0UkpaL0kiLCJ1aW4iOiIzNTEzMDIxNDYifQ%3D%3D&data=fpHkTsCy-oCPE1bMwmeYvBxo2eENJc3ZtVqXxScjLvmMYyUBVtM1A1D-qP7cSB3avC-8Gs4dh7lNXSbcvZpUhA&svctype=4&tempid=h5_group_info)<br>
 
-#### 使用说明
+</div>
 
-1.  xxxx
-2.  xxxx
-3.  xxxx
+### 图库说明
 
-#### 参与贡献
+**本仓库仅为介绍说明，图片的存储库为[图库](https://gitee.com/herijian/characterpic1)**
 
-1.  Fork 本仓库
-2.  新建 Feat_xxx 分支
-3.  提交代码
-4.  新建 Pull Request
+啬图有小部分
 
+图库会保证每个版本角色的面板图，新角色图源少会更新慢，后期老角色图片也会有更新及筛选调整
 
-#### 特技
+### 食用方法
 
-1.  使用 Readme\_XXX.md 来支持不同的语言，例如 Readme\_en.md, Readme\_zh.md
-2.  Gitee 官方博客 [blog.gitee.com](https://blog.gitee.com)
-3.  你可以 [https://gitee.com/explore](https://gitee.com/explore) 这个地址来了解 Gitee 上的优秀开源项目
-4.  [GVP](https://gitee.com/gvp) 全称是 Gitee 最有价值开源项目，是综合评定出的优秀开源项目
-5.  Gitee 官方提供的使用手册 [https://gitee.com/help](https://gitee.com/help)
-6.  Gitee 封面人物是一档用来展示 Gitee 会员风采的栏目 [https://gitee.com/gitee-stars/](https://gitee.com/gitee-stars/)
+ **方便更新** 
+
+在yunzai根目录下执行以下任一命令
+
+使用gitee
+```
+git clone https://gitee.com/herijian/characterpic1.git ./plugins/miao-plugin/resources/profile/
+```
+
+使用github
+
+```
+git clone https://github.com/herijian1/characterpic1.git ./plugins/miao-plugin/resources/profile/
+```
+
+使用gitcode
+
+```
+git clone https://gitcode.com/herijian/characterpic1.git ./plugins/miao-plugin/resources/profile/
+```
+
+若克隆仓库失败，先删除miao-plugin/resources/profile目录下原有的normal-character文件夹
+
+### 更新方法
+
+这几个方法除了1都挺方便的
+
+**方法1**
+更新时进入目录`Yunzai/plugins/miao-plugin/resources/profile`后执行git pull即可
+
+**方法2**
+直接给机器人发 `#更新miao-plugin/resources/profile`喵崽不知道是否可行
+
+**方法3**
+安装面板图库更新插件，yunzai根目录执行
+```
+curl -o "./plugins/example/面板图库更新.js" "https://gitee.com/herijian/characterpic/raw/master/面板图库更新.js"
+```
+发送#更新面板图库
+> Tip：如果未安装面板图库无法使用，请确保已克隆面板图仓库
+
+**方法4**
+如果是linux系统，有trss插件可以直接使用rc指令安装，对机器人发送
+```
+rc rm -r plugins/miao-plugin/resources/profile/normal-character && cd plugins/miao-plugin/resources/profile && git clone https://gitee.com/herijian/characterpic1.git
+```
+该命令中使用的仓库地址可更换为github，gitcode源
+
+更新时对机器人发送
+```
+rc cd plugins/miao-plugin/resources/profile && git pull
+```
+
+ ### 手动安装方法（不方便更新）
+
+随便找个地方克隆图库 或 直接下载压缩包
+```
+git clone https://gitee.com/herijian/characterpic1.git
+```
+
+### 存放位置
+手动方法下载的将normal-character文件夹放在Yunzai/plugins/miao-plugin/resources/profile目录下
+
+**可选** 彩蛋图存放位置
+Yunzai/plugins/miao-plugin/resources/profile/super-character
+
+### 免责声明
+* **请勿将此模板图库用于任何以盈利为目的的场景.** 
+* **图片与其他素材均来自于网络，如有侵权请联系，会立即删除.**
+
+### 联系方式
+
+* QQ：[351302146](https://qm.qq.com/q/3Li0J4Vuze)
+
+* 群号：[768216689](https://qun.qq.com/universal-share/share?ac=1&authKey=OhXBwpnRd2oyFNIbE%2BItRHYAGj8JpSiWFVBnb4tEz6lJzXxnRzQytnMaHSEWFE4z&busi_data=eyJncm91cENvZGUiOiI3NjgyMTY2ODkiLCJ0b2tlbiI6ImNsYzl2MFNBSGxkOWFJcDJ5YnpKam9BZFZXdkFOZ0MveVliZE1pU0VVbHNqQnZGdlYvSTRvcmpPbUs0UkpaL0kiLCJ1aW4iOiIzNTEzMDIxNDYifQ%3D%3D&data=fpHkTsCy-oCPE1bMwmeYvBxo2eENJc3ZtVqXxScjLvmMYyUBVtM1A1D-qP7cSB3avC-8Gs4dh7lNXSbcvZpUhA&svctype=4&tempid=h5_group_info)
+
+* 投喂：[爱发电](https://afdian.com/a/herijian)
+
+**点点⭐Star⭐吧**
---
+++ 面板图库更新.js
@@ -0,0 +1,149 @@
+import { exec } from 'child_process'
+import path from 'path'
+
+// 获取绝对路径
+const profilePath = path.resolve(process.cwd(), 'plugins/miao-plugin/resources/profile')
+
+export class updateProfile extends plugin {
+  constructor() {
+    super({
+      name: '何日见面板图库更新',
+      dsc: '何日见面板图更新',
+      event: 'message',
+      priority: 1,
+      rule: [
+        {
+          reg: '^#更新(面板图|图库|面板图库)$',
+          fnc: 'updateProfile'
+        },
+        {
+          reg: '^#强制更新(面板图|图库|面板图库)$',
+          fnc: 'forceUpdateProfile'
+        },
+        {
+          reg: '^#(面板图|图库|面板图库)更新日志$',
+          fnc: 'updateLog'
+        }
+      ]
+    })
+  }
+
+  async checkMaster(e) {
+    if (!e.isMaster) {
+      console.log(`[面板图更新] 无权限用户 ${e.user_id} 尝试执行命令`)
+      e.reply('⚠去你丫的，什么人啊命令我')
+      return false
+    }
+    console.log(`[面板图更新] 主人 ${e.user_id} 执行命令: ${e.msg}`)
+    return true
+  }
+
+  // 执行Git命令并处理结果
+  async executeGitCommand(e, command, actionName, isForce = false) {
+    return new Promise((resolve) => {
+      console.log(`[面板图更新] 执行命令: ${command} (目录: ${profilePath})`)
+      
+      exec(command, { cwd: profilePath }, (error, stdout, stderr) => {
+        const output = stdout + stderr
+        console.log(`[面板图更新] 命令输出:\n${output}`)
+        
+        // 检测是否为最新状态
+        const isUpToDate = /Already up to date|已经是最新的|Already up-to-date|up to date/i.test(output)
+        
+        // 解析文件改动数量
+        const filesChanged = output.match(/(\d+) files? changed/) || [0]
+        const changedCount = parseInt(filesChanged[1]) || 0
+        
+        // 错误处理
+        if (error) {
+          const errorCode = `ERR_${error.code || 'GIT_FAILED'}`
+          const errorMsg = `${actionName}失败！\nError: ${errorCode}\n详情: ${error.message || stderr || '未知错误'}\n可以尝试执行#强制更新面板图库`
+          
+          console.log(`[面板图更新] 命令执行失败: ${errorMsg}`)
+          e.reply(errorMsg)
+          return resolve(false)
+        }
+        
+        // 结果处理
+        let resultMsg
+        if (isUpToDate) {
+          resultMsg = '图库已是最新，无需再次更新！'
+          console.log('[面板图更新] 图库已是最新状态')
+        } else if (changedCount > 0) {
+          resultMsg = `${isForce ? '强制更新' : '更新'}成功！共更新了${changedCount}张图片`
+          console.log(`[面板图更新] 共${changedCount}个文件改动`)
+        } else {
+          resultMsg = `${isForce ? '强制更新' : '更新'}成功！没有图片被更新`
+          console.log('[面板图更新] 未检测到文件改动')
+        }
+        
+        e.reply(resultMsg)
+        console.log(`[面板图更新] 操作完成: ${resultMsg}`)
+        resolve(true)
+      })
+    })
+  }
+
+  async getLog() {
+    return new Promise((resolve) => {
+      const cmd = 'git log -100 --pretty="%h||[%cd] %s" --date=format:"%F %T"'
+      exec(cmd, { cwd: profilePath }, (error, stdout) => {
+        if (error) return resolve(`面板图库日志查询失败：${error.message}`)
+        
+        const logAll = stdout.split("\n").filter(str => str.trim())
+        if (!logAll.length) return resolve("面板图库暂无更新日志")
+        
+        let log = []
+        for (let str of logAll) {
+          const splitStr = str.split("||")
+          if (splitStr[1] && !splitStr[1].includes("Merge branch")) {
+            log.push(splitStr[1])
+          }
+        }
+        
+        if (log.length <= 0) return resolve("")
+        
+        exec("git config -l", { cwd: profilePath }, (err, configStdout) => {
+          let remoteUrl = ""
+          if (!err) {
+            const urlMatches = configStdout.match(/remote\..*?\.url=.+/g) || []
+            for (const match of urlMatches) {
+              const url = match.replace(/remote\..*?\.url=/g, "").replace(/\/\/([^@]+)@/, "//")
+              remoteUrl = url
+              break
+            }
+          }
+          
+          const msg = [`面板图库更新日志，共${log.length}条`, log.join("\n\n")]
+          if (remoteUrl) msg.push(remoteUrl)
+          resolve(Bot.makeForwardArray(msg))
+        })
+      })
+    })
+  }
+
+  async updateLog(e) {
+    if (!(await this.checkMaster(e))) return
+    const log = await this.getLog()
+    await e.reply(log)
+  }
+
+  // 更新
+  async updateProfile(e) {
+    if (!(await this.checkMaster(e))) return
+    
+    e.reply('开始更新面板图库...')
+    console.log('[面板图更新] 更新图库')
+    await this.executeGitCommand(e, 'git pull', '图库更新')
+  }
+
+  // 强制更新
+  async forceUpdateProfile(e) {
+    if (!(await this.checkMaster(e))) return
+    
+    e.reply('开始强制更新面板图库...')
+    console.log('[面板图更新] 强制更新图库')
+    const resetCmd = 'git checkout . && git clean -fd && git pull'
+    await this.executeGitCommand(e, resetCmd, '强制更新', true)
+  }
+}
